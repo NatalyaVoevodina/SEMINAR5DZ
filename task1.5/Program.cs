eada61d4d8ffc7501c6730c65c0f8842ddc93a3e
natlya_voevodina <voevodochka1988@yandex.ru>
2023-03-02 21:30:00 +0300

    task1.5

---
+++ task1.5/Program.cs
@@ -0,0 +1,33 @@
+﻿//*Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.*/
+
+
+void FillArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(99, 1000);
+
+}
+
+void PrintArray (int [] array)
+{
+foreach (var el in array)
+ Console.Write($"{el} ");
+  Console.WriteLine();
+}
+
+int CountEvNum(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+    if (array[i] % 2 == 0)
+      count++;
+      ;
+  return count;
+}
+
+int[] array = new int [8];
+FillArray(array);
+PrintArray (array);
+Console.WriteLine ($"количество четых чисел в сгенеррированном массиве -> {CountEvNum(array)}");
+
+
